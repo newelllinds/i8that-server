839b1a64e1c91e680051a8484b3a2e0cfb87439a
Kquanisha <kdixon8709@yahoo.com>
2021-02-15 21:00:45 -0500

    Updated Changes

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -38,25 +38,24 @@ router.post("/login", function (req, res) {
   })
     .then(function loginSuccess(user) {
       if (user) {
-        bcrypt,
-          compare(req.body.user.password, user.password, function (
-            err,
-            matches
-          ) {
-            if (matches) {
-              let token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
-                expiresIn: 60 * 60 * 24,
-              });
+        bcrypt.compare(req.body.user.password, user.password, function (
+          err,
+          matches
+        ) {
+          if (matches) {
+            let token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+              expiresIn: 60 * 60 * 24,
+            });
 
-              res.status(200).json({
-                user: user,
-                message: "User successfully logged in!",
-                sessionToken: token,
-              });
-            } else {
-              res.status(502).send({ error: "Login failed" });
-            }
-          });
+            res.status(200).json({
+              user: user,
+              message: "User successfully logged in!",
+              sessionToken: token,
+            });
+          } else {
+            res.status(502).send({ error: "Login failed" });
+          }
+        });
       } else {
         res.status(500).json({ error: "User does not exist." });
       }
