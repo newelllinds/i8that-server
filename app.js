149c9fa7fa54da8c9c9565c048489528ba0d1601
Kquanisha <kdixon8709@yahoo.com>
2021-02-15 19:12:12 -0500

    Updated Usercontroller

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-let express = require('express');
+require("dotenv").config();
+let express = require("express");
 let app = express();
-let sequelize = require('./db');
+let sequelize = require("./db");
 
-let dietlog = require('./controllers/dietlogcontroller');
-let user = require('./controllers/usercontroller');
+let dietlog = require("./controllers/dietlogcontroller");
+let user = require("./controllers/usercontroller");
 
 sequelize.sync();
 
-app.use(require('./middleware/headers'));
+app.use(require("./middleware/headers"));
 
-app.use('/user', user);
-app.use('/dietlog', dietlog)
+app.use(express.json());
 
-app.listen(3000, function(){
-    console.log("App is listening on port 3000")
-})
+app.use("/user", user);
+app.use("/dietlog", dietlog);
+
+app.listen(3000, function () {
+  console.log("App is listening on port 3000");
+});
